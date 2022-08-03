66362c4c73fbb4e9a6bb9f1a34bf4616b189aef3
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-08-04 00:00:36 +0300

    some-styling

--- meals-app/screens/MealsOverviewScreen.js
+++ meals-app/screens/MealsOverviewScreen.js
@@ -1,9 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, Text, StyleSheet } from "react-native";
 import { MEALS } from "../data/dummy-data";
-function MealsOverviewScreen() {
+import MealItem from "../components/MealItem";
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      durability: item.durability,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text> asdasd</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      ></FlatList>
     </View>
   );
 }
