2a8c38c8b4ecacb33d7127f935477869041e3306
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-08-05 18:20:53 +0300

    context-api

--- meals-app/App.js
+++ meals-app/App.js
@@ -7,42 +7,46 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import FavoriteScreen from "./screens/FavoritesScreen";
+import FavoritesContextProvider from "./store/context/favorites-context";
+
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-// function Drawernavigator() {
-//   return (
-//     <Drawernavigator>
-//       <Drawer.Screen name="Categories" component={CategoriesScreen} />
-//       <Drawer.Screen name="Favorites" component={FavoriteScreen} />
-//     </Drawernavigator>
-//   );
-// }
+function Drawernavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen} />
+      <Drawer.Screen name="Favorites" component={FavoriteScreen} />
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
     <>
-      <StatusBar style="dark" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backGroundColor: "white" },
-            headerTintColor: "black",
-            contentStyle: { backgroundColor: "white" },
-          }}
-        >
-          <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{ title: "All Categories" }}
-          />
-          <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverviewScreen}
-            options={{ title: "Meals" }}
-          />
-          <Stack.Screen name="MealDetail" component={MealDetailComponent} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <FavoritesContextProvider>
+        <StatusBar style="dark" />
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { backGroundColor: "white" },
+              headerTintColor: "black",
+              contentStyle: { backgroundColor: "white" },
+            }}
+          >
+            <Stack.Screen
+              name="MealsCategories"
+              component={Drawernavigator}
+              options={{ title: "All Categories" }}
+            />
+            <Stack.Screen
+              name="MealsOverview"
+              component={MealsOverviewScreen}
+              options={{ title: "Meals" }}
+            />
+            <Stack.Screen name="MealDetail" component={MealDetailComponent} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FavoritesContextProvider>
     </>
   );
 }
--- meals-app/screens/MealDetailScreen.js
+++ meals-app/screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import {
   Text,
   View,
@@ -12,14 +12,22 @@ import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
+import { FavoritesContext } from "../store/context/favorites-context";
 
 function MealDetailScreen({ route, navigation }) {
+  const favoriteMealCtx = useContext(FavoritesContext);
+
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const mealIsFavorite = favoriteMealCtx.ids.includes(mealId);
 
-  function headerButtonPressHandler() {
-    console.log("asdasdasdas");
+  function changeFavoriteStatusHandler() {
+    if (mealIsFavorite) {
+      favoriteMealCtx.removeFavorite(mealId);
+    } else {
+      favoriteMealCtx.addFavorite(mealId);
+    }
   }
 
   useLayoutEffect(() => {
@@ -27,15 +35,15 @@ function MealDetailScreen({ route, navigation }) {
       headerRight: () => {
         return (
           <IconButton
-            icon="star"
+            icon={mealIsFavorite ? "star" : "star-outline"}
             color="black"
-            onPress={headerButtonPressHandler}
+            onPress={changeFavoriteStatusHandler}
           />
         );
       },
     });
   }),
-    [navigation, headerButtonPressHandler];
+    [navigation, changeFavoriteStatusHandler];
 
   return (
     <ScrollView style={styles.rootContainer}>
